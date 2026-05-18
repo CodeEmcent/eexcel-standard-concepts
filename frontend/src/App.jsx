53101593aa69288fd6f8890c95a1bb167ad0f30d
Emcent <innocent@emcent.dev>
2026-05-18 15:19:45 +0100

    fix(frontend): scroll to top on page navigation Closes #30

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar       from './components/layout/Navbar';
 import Footer       from './components/layout/Footer';
+import ScrollToTop  from './components/common/ScrollToTop';
 import Home         from './pages/Home';
 import Catalogue    from './pages/Catalogue';
 import About        from './pages/About';
@@ -9,6 +10,7 @@ import Contact      from './pages/Contact';
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-1">
